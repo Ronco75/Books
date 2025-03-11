b9c84930445064fe537ab5b528d6c54be1244862
Ron Cohen <ronc7500@gmail.com>
2025-03-11 13:22:17 +0200

    Add Service

---
+++ src/main/java/ronco/books/service/impl/BookServiceImpl.java
@@ -0,0 +1,8 @@
+package ronco.books.service.impl;
+
+import org.springframework.stereotype.Service;
+import ronco.books.service.BookService;
+
+@Service
+public class BookServiceImpl implements BookService {
+}
